c77b0bbd4ec19aff953835fd9c875987b7712981
throwaway979 <52045975+throwaway979@users.noreply.github.com>
2019-06-20 19:59:38 +0300

    Update FixAnimations.cpp

--- wanheda/Features/Animations/FixAnimations.cpp
+++ wanheda/Features/Animations/FixAnimations.cpp
@@ -9,7 +9,7 @@ void __fastcall animations::UpdateClientSideAnimations(uintptr_t ecx, uintptr_t 
 	auto e = (C_BaseEntity*)ecx;
 	auto original = g_Anim.m_track[e->EntIndex()].m_vmt->get_old_method< fn::UpdateClientSideAnimations_t >(221);
 
-	if (g::AllowAnimationUpdate[e->EntIndex()] || !g_Menu.Config.PosBacktrack || e == g::pLocalEntity)
+//	if (g::AllowAnimationUpdate[e->EntIndex()] || !g_Menu.Config.PosBacktrack || e == g::pLocalEntity)
 		original(ecx);
 
 	return;
@@ -35,9 +35,60 @@ bool __fastcall animations::SetupBones(uintptr_t ecx, uintptr_t edx, matrix3x4_t
 	auto entity = reinterpret_cast <C_BaseEntity*>(ecx - 4);
 	auto original = g_Anim.m_track[entity->EntIndex()].m_renderable_vmt->get_old_method< fn::SetupBones_t >(13);
 
-//antipasta yet again
+	if (!entity || entity->GetClientClass()->ClassID != (int)EClassIds::CCSPlayer|| !g_Menu.Config.PosBacktrack)
+		return original(ecx, bone_to_world_out, max_bones, bone_bask, current_time);
 
-	 original(ecx, bone_to_world_out, max_bones, bone_bask, current_time);
+	if (bone_bask == 0x00000100)
+		bone_bask |= 0x200;
+	if (current_time == g_pEngine->GetLastTimeStamp())
+		current_time += 3.5f;
+//readded cuz tommy wanted it in yet again
+	//backup
+	static auto host_timescale = g_pCvar->FindVar(("host_timescale"));
+	float OldCurTime = g_pGlobalVars->curtime;
+	float OldRealTime = g_pGlobalVars->realtime;
+	float OldFrameTime = g_pGlobalVars->frametime;
+	float OldAbsFrameTime = g_pGlobalVars->absoluteframetime;
+	float OldAbsFrameTimeStart = g_pGlobalVars->absoluteframestarttimestddev;
+	float OldInterpAmount = g_pGlobalVars->interpolationAmount;
+	int OldFrameCount = g_pGlobalVars->framecount;
+	int OldTickCount = g_pGlobalVars->tickcount;
+
+	g_pGlobalVars->curtime = entity->GetSimulationTime();
+	g_pGlobalVars->realtime = entity->GetSimulationTime();
+	g_pGlobalVars->frametime = g_pGlobalVars->intervalPerTick * host_timescale->GetFloat();
+	g_pGlobalVars->absoluteframetime = g_pGlobalVars->intervalPerTick * host_timescale->GetFloat();
+	g_pGlobalVars->absoluteframestarttimestddev = entity->GetSimulationTime() - g_pGlobalVars->intervalPerTick * host_timescale->GetFloat();
+	g_pGlobalVars->interpolationAmount = 0;
+	g_pGlobalVars->framecount = TIME_TO_TICKS(entity->GetSimulationTime());
+	g_pGlobalVars->tickcount = TIME_TO_TICKS(entity->GetSimulationTime());
+
+	static int m_fFlags = g_pNetvars->GetOffset("DT_BasePlayer", "m_fFlags");
+	auto backup = *(int*)(entity + 2600);
+
+
+	*(int*)(entity + 2600) |= 0xA;
+	*(int*)((int)entity + 236) |= 8; // IsNoInterpolationFrame
+	*reinterpret_cast<int*>(uintptr_t(entity) + m_fFlags) |= 0xF0;
+	bool ret_value = original(ecx, bone_to_world_out, max_bones, bone_bask, current_time);
+	*reinterpret_cast<int*>(uintptr_t(entity) + m_fFlags) &= ~(0xF0);
+	*reinterpret_cast<int*>(uintptr_t(entity) + m_fFlags) |= 0x98;
+	*(int*)(entity + 2600) = backup;
+	*(int*)((int)entity + 236) &= ~8; // (1 << 3)
+
+	//restore old values
+	g_pGlobalVars->curtime = OldCurTime;
+	g_pGlobalVars->realtime = OldRealTime;
+	g_pGlobalVars->frametime = OldFrameTime;
+	g_pGlobalVars->absoluteframetime = OldAbsFrameTime;
+	g_pGlobalVars->absoluteframestarttimestddev = OldAbsFrameTimeStart;
+	g_pGlobalVars->interpolationAmount = OldInterpAmount;
+	g_pGlobalVars->framecount = OldFrameCount;
+	g_pGlobalVars->tickcount = OldTickCount;
+
+	return ret_value;
+
+	//return g_SetupBones.HandleBoneSetup(entity, bone_to_world_out, bone_bask, current_time);
 }
 
 void c_animations::init() {
